f5fda151c9ddf7998c608b7bf78d1e6bf1e77fa8
Agentzhao <tan.hongzhao@dhs.sg>
2020-06-28 15:29:25 +0800

    Create ttt.java

---
+++ ttt.java
@@ -0,0 +1,126 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.print("Enter cells: ");
+        String[] ttt = new String[9];
+        String temp = scanner.next();
+       
+        for (int i=0; i < 9;i++){
+           ttt[i] = temp.substring(i,i+1);
+        }
+       
+        //print
+        System.out.println("---------");
+        System.out.println("| " + ttt[0] + " " + ttt[1] + " " + ttt[2] + " |");
+        System.out.println("| " + ttt[3] + " " + ttt[4] + " " + ttt[5] + " |");
+        System.out.println("| " + ttt[6] + " " + ttt[7] + " " + ttt[8] + " |");
+        System.out.println("---------");
+        
+        //analyzer
+        boolean xwins = false;
+        boolean owins = false;
+        boolean notfinished = false;
+        boolean impossible = false;
+        
+        int countx = 0;
+        int counto = 0;
+        
+        //check if game not finished
+        for (int i=0;i<9;++i){
+            if (ttt[i].equals("X")){
+                countx = countx + 1;
+            } else if (ttt[i].equals("O")){
+                counto = counto + 1;
+            } else {
+                notfinished = true;
+            }
+        }
+        
+        if (-1 > (counto - countx) || (counto - countx) > 1){
+            impossible = true;     
+        }
+        
+        if (ttt[0].equals(ttt[1]) && ttt[1].equals(ttt[2])){
+            if (ttt[0].equals("X")){
+                xwins = true;
+            }
+            if (ttt[0].equals("O")){
+                owins = true;
+            }
+        }
+              
+        if (ttt[3].equals(ttt[4]) && ttt[4].equals(ttt[5])){
+            if (ttt[3].equals("X")){
+                xwins = true;
+            }
+            if (ttt[3].equals("O")){
+                owins = true;
+            }
+        }
+        if (ttt[6].equals(ttt[7]) && ttt[7].equals(ttt[8])){
+            if (ttt[6].equals("X")){
+                xwins = true;
+            }
+            if (ttt[6].equals("O")){
+                owins = true;
+            }
+        }
+        if (ttt[0].equals(ttt[3]) && ttt[3].equals(ttt[6])){
+            if (ttt[0].equals("X")){
+                xwins = true;
+            }
+            if (ttt[0].equals("O")){
+                owins = true;
+            }
+        }
+        if (ttt[1].equals(ttt[4]) && ttt[4].equals(ttt[7])){
+            if (ttt[1].equals("X")){
+                xwins = true;
+            }
+            if (ttt[1].equals("O")){
+                owins = true;
+            }
+        }
+        if (ttt[2].equals(ttt[5]) && ttt[5].equals(ttt[8])){
+            if (ttt[2].equals("X")){
+                xwins = true;
+            }
+            if (ttt[2].equals("O")){
+                owins = true;
+            }
+        }
+        if (ttt[0].equals(ttt[4]) && ttt[4].equals(ttt[8])){
+            if (ttt[0].equals("X")){
+                xwins = true;
+            }
+            if (ttt[0].equals("O")){
+                owins = true;
+            }
+        }
+        if (ttt[2].equals(ttt[4]) && ttt[4].equals(ttt[6])){
+            if (ttt[2].equals("X")){
+                xwins = true;
+            }
+            if (ttt[2].equals("O")){
+                owins = true;
+            }
+        }
+        
+        if (xwins && owins){
+            System.out.println("Impossible");
+        } else if (xwins){
+            System.out.println("X wins");
+        } else if (owins){
+            System.out.println("O wins");
+        } else if (notfinished){
+            System.out.println("Game not finished");
+        } else {
+            System.out.println("Draw");
+        }
+    }
+}
